611fc1939d85ea01b62bc2e086b0297057f20c0e
Tran Trong Binh <tranbinhbak@gmail.com>
2018-11-28 00:21:50 +0700

    Page profile team

--- resources/views/particals/top-info.blade.php
+++ resources/views/particals/top-info.blade.php
@@ -1,27 +1,90 @@
-<div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <div class="row text-center">
-            <div class="col-md-12">
-			    <div class="profile-page">
-			        <div class="wrapper">
-			            <div class="page-header page-header-small" filter-color="green">
-			                <div class="page-header-image" data-parallax="true" style="background-image: url('img/bg.jpg');"></div>
-			                <div class="container">
-			                    <div class="content-center">
-			                        <div class="cc-profile-image"><a href="#"><img src="img/user.jpg" alt="Image"/></a></div>
-			                        <div class="h2 title">Anthony Barnett</div>
-			                        <p class="category text-white">Web Developer, Graphic Designer, Photographer</p><a class="btn btn-primary smooth-scroll mr-2" href="#contact" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Hire Me</a><a class="btn btn-primary" href="#" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Download CV</a>
-			                    </div>
-			                </div>
-			                <div class="section">
-			                    <div class="container">
-			                        <div class="button-container"><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Facebook"><i class="fa fa-facebook"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Twitter"><i class="fa fa-twitter"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Google+"><i class="fa fa-google-plus"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Instagram"><i class="fa fa-instagram"></i></a></div>
-			                    </div>
-			                </div>
-			            </div>
-			        </div>
-			    </div>
-            </div>
+<!-- <div class="jumbotron jumbotron-fluid"> -->
+<div class="penfolio">
+  <div class="penfolio_inner">
+    <section class="penfolio_inner__home" scroll_target="home">
+      <div class="header_bg"></div>
+      <header>
+        <div class="pen_logo">
+          <h2>Penfolio</h2>
         </div>
+        <div class="pen_cta">
+          <a class="p_link" href="#" target="blank">Follow me on codepen</a>
+        </div>
+      </header>
+      <div class="pen_intro">
+        <div class="pen_intro__image">
+          <a class="p_link" href="#" target="blank">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/profile/profile-512.jpg?1540230348">
+          </a>
+        </div>
+        <h2>Jamie Coulter</h2>
+        <h3>@jcoulterdesign</h3>
+        <h4>3,768 Followers | 17 Following</h4>
+        <h5>Build a sweet portfolio in seconds. Simply change the codepen_username variable in the JS and boom! Instant portfolio!</h5>
+        <nav>
+          <ul>
+            <li class="active">Home</li>
+            <li>About</li>
+            <li>Penfolio</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+    <div class="penfolio_inner__about" scroll_target="about">
+      <div class="about_left">
+        <h2>About me</h2>
+      </div>
+      <div class="about_right">
+        <p class="headline">Penfolio is a fully customisable HTML template that integrates directly with your pens. Built using <a href="http://cpv2api.com/">Nate Wileys</a> amazing API.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at imperdiet lorem. Sed vitae sodales nulla. Etiam tincidunt cursus augue vel posuere. Aenean justo arcu, hendrerit non lacinia efficitur, viverra vitae mi. In blandit finibus ex a maximus. Vestibulum nec consectetur justo. Nam tempor iaculis metus auctor posuere. Duis vulputate ipsum non ante mattis, in cursus diam elementum. Nam hendrerit mauris ut <a href="http://cpv2api.com/">mauris</a> viverra ornare.</p>
+        <p>Mauris augue lacus, porta nec urna a, malesuada egestas nisi. Nam est nunc, commodo et neque sed, ornare ullamcorper elit. Fusce volutpat libero eu lacus varius iaculis. Donec erat nisi, fermentum eu lacinia quis, blandit non orci. Maecenas pharetra libero eu tempor ornare. <a href="http://cpv2api.com/">Nullam</a> id tortor dictum, euismod lacus sagittis, ullamcorper justo. Pellentesque sodales mi sit amet turpis accumsan, sed consequat est aliquet.</p>
+        <div class="pen_cta">
+          <a class="p_link" href="#" target="blank">Follow me on codepen</a>
+        </div>
+      </div>
     </div>
+    <div class="penfolio_inner__pens" scroll_target="penfolio">
+      <div class="penfolio_header">
+        <div class="header_bg" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/penfolio_bg.png"></div>
+        <h2>Penfolio</h2>
+        <h3>
+          The penfolio of  
+          <span class="rep_username">Jamie Coulter</span>
+        </h3>
+      </div>
+      <div class="wrap">
+      <article class="post">
+	    <h1>First Post</h1>
+	    <div class="meta">by <a href="http://ajiksip.web.id">Aji Kurniawan</a></div>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut sodales est. Donec congue tristique ipsum id rutrum. Praesent mattis accumsan purus eu tristique. Praesent vel volutpat odio. Nullam dictum congue vehicula. Nunc sit amet blandit magna, at ultrices lorem.</p>
+	  </article>
+	  <article class="post">
+	    <h1>Second Post</h1>
+	    <p>Nullam nec magna nisl. Maecenas congue est quis nisl aliquet fringilla. Quisque quis felis vel mauris dignissim elementum. Cras a arcu finibus, convallis turpis eget, faucibus sapien.</p>
+	  </article>
+    </div>
+    <div class="penfolio_inner__contact" scroll_target="contact">
+      <h2>Get in touch</h2>
+      <h3>Go ahead, i don't bite!</h3>
+      <div class="pen_intro__image">
+        <a class="p_link" href="#" target="blank">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/profile/profile-512.jpg?1540230348">
+        </a>
+      </div>
+      <div class="rep_username">Jamie Coulter</div>
+      <div class="rep_location">Leeds, United Kingdom</div>
+      <div class="rep_links">
+        <ul><li><a href="http://www.jamiecoulter.co.uk">http://www.jamiecoulter.co.uk</a></li><li><a href="https://twitter.com/jamiecoulter89">https://twitter.com/jamiecoulter89</a></li></ul>
+      </div>
+      <div class="pen_cta">
+        <a class="cp p_link" href="https://www.codepen.io/jcoulterdesign" target="blank">Follow me on codepen</a>
+      </div>
+    </div>
+    <div style="padding-bottom: 100px"></div>
+  </div>
 </div>
+
+
+
+<!-- </div> -->
