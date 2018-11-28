582877c3a687771748ab84c32f5b4c5d72795ec2
Tran Trong Binh <tranbinhbak@gmail.com>
2018-11-28 22:49:28 +0700

    fixed box

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,3 +5,32 @@
 		@include('widgets.article')
 	</div>
 @endsection
+
+@section('scripts')
+<script>
+           
+	// $(document).ready(function () {
+ //        var sidebarheight = $('#right').height();
+ //        var windowheight = $(window).height();
+
+
+ //        $(window).scroll(function () {
+ //            var scrollTop = $(window).scrollTop();
+
+ //            if (scrollTop >= sidebarheight - windowheight){
+ //                $('#right').addClass('is-bottomPosition');
+ //            }
+ //            else {
+ //                $('#right').removeClass('is-bottomPosition');
+ //            }                   
+ //        });
+ //    });
+ 		$(window).bind('scroll', function() {
+		    if($(window).scrollTop() >= $('#right').offset().top + $('#right').outerHeight() - window.innerHeight) {
+		        $('#right').addClass('is-bottomPosition');
+		    }
+		});
+
+
+</script>
+@endsection
